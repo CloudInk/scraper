ad81befabb936888ad71b7e68ed3a81ff79a3986
tim-walton <timdwalton@gmail.com>
2015-11-14 01:08:28 -0600

    Update scraper-view.php

--- scraper-view.php
+++ scraper-view.php
@@ -1,18 +1,22 @@
-
 <?php
 include('scraper.php');
 $s = new scraper();
 
-$s->scrape();
-
-if(isset($_GET['json'])) {
-    exit(json_encode($s->articles));
+if (isset($_GET['json'])) {
+    exit($s->scrapeIndex()->scrapeIndexArticles()->printJSONScrapes());
 }
 
+$s->scrapeIndex()->scrapeIndexArticles();
+
+
 ?>
+
+
 <!doctype html>
-<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
-<!--[if IE 10]><html class="ie10" lang="en" > <![endif]-->
+<!--[if IE 9]>
+<html class="lt-ie10" lang="en"> <![endif]-->
+<!--[if IE 10]>
+<html class="ie10" lang="en"> <![endif]-->
 <html class="no-js" lang="en" data-useragent="Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)">
 <head>
     <meta charset="utf-8"/>
@@ -26,35 +30,45 @@ if(isset($_GET['json'])) {
 <div class="row">
     <div class='small-12 columns' style=''>
         <ul class='pricing-table'>
-            <li class='title'>Scraper v1 - MSNBC Version <br><small><a href="scraper-view.php?json=1">View JSON Output</a></small> </li>
+            <li class='title'>Scraper v1 - MSNBC Version <br>
+                <small><a href="scraper-view.php?json=1">View JSON Output</a></small>
+            </li>
 
         </ul>
     </div>
-        <?php
-        foreach($s->articles as $a) {
-            $jsona[] = json_encode($a);
-        }
-                    if(is_array($s->articles)) {
-                        $x = 0;
-                        foreach($s->articles as $article) {
+    <?php
+    if (is_array($s->articles)) {
+        $x = 0;
+        foreach ($s->articles as $article) {
 
-                            echo "
+            echo "
 
-                             <div class='small-6 columns left-align' style=''>
-                                <ul class='pricing-table' style='height: 477px;'>
+                             <div class='small-12 columns' style=''>
+                                <ul class='pricing-table' style='height: auto;'>
                                     <li class='title'>{$article['article-title']}<br><small>{$s->url}</small></li>
                                     <li class='bullet-item'><img src='{$article['article-image-src']}' style='border: 2px #ccc solid; height: 200px; width:  400px;'></small></li>
                                     <li class='bullet-item'><small style='font-size:0.8em;'>{$article['article-image-text']}</small></li>
-                                    <li class='cta-button'><a class='button' href='{$article['article-link']}' target='_blank'>Read Article</a></li>
+                                    <li class='cta-button'>";
+
+            if (is_array($article['article-body'])) {
+                foreach ($article['article-body'] as $line) {
+                    echo "<p style='text-align: left;'>{$line}</p>";
+                }
+            } else {
+                echo "<a class='button' href='{$article['article-link']}'>Watch Video</a>";
+            }
+
+
+            echo "</li>
                                 </ul>
                             </div>
 
                             ";
-                            $x++;
-                        }
-                    }
+            $x++;
+        }
+    }
 
-                ?>
+    ?>
     <div class='small-12 columns' style=''>
         <ul class='pricing-table'>
             <li class='title'>timdwalton@gmail.com | 316.993.3274</li>
@@ -72,4 +86,3 @@ if(isset($_GET['json'])) {
 </script>
 </body>
 </html>
-
